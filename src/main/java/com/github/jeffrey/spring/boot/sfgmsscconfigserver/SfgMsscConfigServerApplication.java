c11788d7d67937b8e439843ea4296473d4ac58cc
jeffreymzd <mzhde@hotmail.com>
2020-04-05 22:52:45 -0400

    Initial project add

---
+++ src/main/java/com/github/jeffrey/spring/boot/sfgmsscconfigserver/SfgMsscConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.github.jeffrey.spring.boot.sfgmsscconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgMsscConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgMsscConfigServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/jeffrey/spring/boot/sfgmsscconfigserver/SfgMsscConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jeffrey.spring.boot.sfgmsscconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgMsscConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
